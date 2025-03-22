960efa716361776932a91145746c26525d61701e
Mazhar Altincay <mazharaltincay89@gmail.com>
2025-03-22 21:51:51 +0100

    inital start

---
+++ src/test/java/org/example/tpo03_02/Tpo0302ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpo03_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo0302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
